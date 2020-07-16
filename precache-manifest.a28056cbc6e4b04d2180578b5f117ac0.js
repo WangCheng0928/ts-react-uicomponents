7cf97ff6c03b8016a778acd6b0852cac4b77b3dd
WangCheng4934 <493475850@qq.com>
2020-07-16 21:43:37 +0800

    upload no background

---
+++ precache-manifest.a28056cbc6e4b04d2180578b5f117ac0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811b6601d9db91a74bf0264251287db5",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.0624a533e435db60af1c.bundle.js"
+  },
+  {
+    "url": "runtime~main.0624a533e435db60af1c.bundle.js"
+  },
+  {
+    "revision": "ee01942a98ea01f298f3",
+    "url": "static/css/main.b9d80641.chunk.css"
+  },
+  {
+    "revision": "fe16c8252263a516a1d1",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "revision": "203958f3ce9a6ac6443f3de257984c76",
+    "url": "static/media/upload.203958f3.svg"
+  },
+  {
+    "url": "vendors~main.0624a533e435db60af1c.bundle.js"
+  },
+  {
+    "url": "vendors~main.0624a533e435db60af1c.bundle.js.LICENSE.txt"
+  }
+]);
